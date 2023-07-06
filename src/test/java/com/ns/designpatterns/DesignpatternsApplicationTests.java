9a195d6377accbee3962d00141c6631d4a04ea47
Narayana Swamy <nswamy751@gmail.com>
2023-07-06 19:41:37 +0530

    adding initial files

---
+++ src/test/java/com/ns/designpatterns/DesignpatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ns.designpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignpatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
